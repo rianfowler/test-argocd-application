eb3817c918f7af09eaa7bd77ae528d8d1e2960f0
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2024-04-25 11:50:21 -0500

    update hello world message for image push (#6)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! this is a test of promotion push');
 });
 
 app.listen(PORT, () => {
